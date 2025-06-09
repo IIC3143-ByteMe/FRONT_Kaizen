acb514b2e2eeed478cb65a15339485562be3d4d0
AntonioDoberti <antonio.doberti@uc.cl>
2025-06-08 23:29:17 -0400

    Refactor GitHub Pages deployment configuration and improve error handling in login

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// Detecta si estamos en GitHub Actions
+const isGitHubPages = process.env.GITHUB_PAGES === 'true'
+
 export default defineConfig({
+  base: isGitHubPages ? '/FRONT_Kaizen/' : '/',
   plugins: [react()],
-  base: 'FRONT_Kaizen', // 👈 reemplaza por el nombre real del repo
 })
